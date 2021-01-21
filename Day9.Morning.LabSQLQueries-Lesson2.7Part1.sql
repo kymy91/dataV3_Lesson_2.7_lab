c39990656c2fb8ec4d6d0a6c2bdf1a33899c1bbc
kymy91 <kymy91@gmail.com>
2021-01-21 19:25:32 +0100

    Answers to Lab

---
+++ Day9.Morning.LabSQLQueries-Lesson2.7Part1.sql
@@ -0,0 +1,63 @@
+/* Lab | SQL Queries - Lesson 2.7 Part 1
+In this activity we are going to do some database maintenance. 
+In the current database we only have information on movies for the year 2006. 
+Now we have received the film catalog for 2020 as well. 
+For this new data we will create another table and bulk insert all the data there. 
+Code to create a new table has been provided below.
+
+We have just one item for each film, and all will be placed in the new table. 
+For 2020, the rental duration will be 3 days, with an offer price of 2.99€ and a replacement cost of 8.99€ (these are all fixed values for all movies for this year). The catalog is in a CSV file named films_2020.csv that can be found at files_for_lab folder.
+
+Instructions
+Add the new films to the database.*/
+
+USE sakila;
+
+SELECT * FROM sakila.films_2020;
+
+
+drop table if exists films_2020;
+CREATE TABLE `films_2020` (
+  `film_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` text,
+  `release_year` year(4) DEFAULT NULL,
+  `language_id` tinyint(3) unsigned NOT NULL,
+  `original_language_id` tinyint(3) unsigned DEFAULT NULL,
+  `rental_duration` int(6),
+  `rental_rate` decimal(4,2),
+  `length` smallint(5) unsigned DEFAULT NULL,
+  `replacement_cost` decimal(5,2) DEFAULT NULL,
+  `rating` enum('G','PG','PG-13','R','NC-17') DEFAULT NULL,
+  PRIMARY KEY (`film_id`),
+  CONSTRAINT FOREIGN KEY (`original_language_id`) REFERENCES `language` (`language_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1003 DEFAULT CHARSET=utf8;
+
+show variables like 'local_infile';
+set global local_infile = 1;
+
+SHOW GLOBAL VARIABLES;
+
+
+ -- IGNACIO CODE -  didnt work
+load data local infile '‪C:/Users/kyear/Documents/Personal/Education/Ironhack/Week2/9SQL/dataV3_Lesson_2.7_lab/files_for_part1/films_2020.csv'
+into table films_2020
+fields terminated by ',';
+
+ -- ANNA CODE - NEITHER WORKED
+load data local infile '‪C:/Users/kyear/Documents/Personal/Education/Ironhack/Week2/9SQL/dataV3_Lesson_2.7_lab/files_for_part1/films_2020.csv'
+into table films_2020
+fields terminated BY ‘,’
+ENCLOSED BY ‘“’
+LINES TERMINATED BY ‘\n’
+(film_id,title,description,release_year,language_id,original_language_id,rental_duration,rental_rate,length,replacement_cost,rating);
+
+
+### added manually
+
+
+/*Update information on rental_duration, rental_rate, and replacement_cost.
+Hint. You might have to use the following commands to set bulk import option to ON:
+show variables like 'local_infile';
+set global local_infile = 1;*
+If bulk import gives an unexpected error, you can also use the data_import_wizard to insert data into the new table.*/
